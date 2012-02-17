1b49a8a6ecb9382361497798e5b4aca8a89b21b4
adam <adam@76b6fbfc-52b4-42b0-bc5b-92661d7d59e0>
2012-02-17 22:54:36 +0000

    friday cleanup

--- hyp-google-maps.php
+++ hyp-google-maps.php
@@ -42,11 +42,74 @@ require(HGM_INCLUDES . 'admin.php');
 
 /* ~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~ */
 
+//add_action( 'show_user_profile', 'my_show_extra_profile_fields' );
+//add_action( 'edit_user_profile', 'my_show_extra_profile_fields' );
+
+function my_show_extra_profile_fields( $user ) { ?>
+
+	<h3>Extra profile information</h3>
+
+	<table class="form-table">
+
+		<tr>
+			<th><label for="hgm-user-geocoder">HGM User Location</label></th>
+
+			<td>
+				<input type="text" name="hgm_user_geocoder" id="hgm-user-geocoder" value="<?php echo esc_attr( get_the_author_meta( 'hgm_user_geocode', $user->ID ) ); ?>" class="regular-text" /><br />
+				<span class="description">Please enter your Twitter username.</span>
+                
+                
+                <?php hgm_geocoder()?>
+                
+			</td>
+		</tr>
+
+	</table>
+<?php }
+
+
+
+add_action( 'personal_options_update', 'my_save_extra_profile_fields' );
+add_action( 'edit_user_profile_update', 'my_save_extra_profile_fields' );
+
+function my_save_extra_profile_fields( $user_id ) {
+
+	if ( !current_user_can( 'edit_user', $user_id ) )
+		return false;
+
+	/* Copy and paste this line for additional fields. Make sure to change 'twitter' to the field ID. */
+	update_usermeta( $user_id, 'hgm_user_geocoder', $_POST['hgm_user_geocoder'] );
+}
+
+
+$hgm_user_geocoder = new HgmUserGeocoder();
+
+
+class HgmUserGeocoder{
+	public function __construct(){
+		add_action('show_user_profile',array(&$this,'print_user_geocoder'),1,2);
+		add_action('edit_user_profile',array(&$this,'print_user_geocoder'),1,2);
+	}
+	public function print_user_geocoder($user){?>
+        <h3>HGM Geocoding</h3>
+        <table class="form-table">
+    		<tr>
+                <th><label for="hgm-user-geocoder">User Location</label></th>
+                <td><?php hgm_geocoder()?></td>
+            </tr>
+        </table>
+    	<?php
+	}
+}
+
+
+
+
 /* Load Api */
 function hgm_load_api(){ $api_loader = new HgmApiLoader(); }
 
 /* Geocoder */
-function hgm_geocoder($args,$shortcode = false){
+function hgm_geocoder($args = '',$shortcode = false){
 	global $hgm_geocoder;
 	if($shortcode) ob_start();
 	$hgm_geocoder = new HgmGeocoder($args);
@@ -58,7 +121,7 @@ function hgm_geocoder($args,$shortcode = false){
 }
 
 /* Map */
-function hgm_map($args,$shortcode = false){
+function hgm_map($args = '',$shortcode = false){
 	global $hgm_maps;
 	if($shortcode) ob_start();
 	$hgm_maps[] = new HgmMap($args);
@@ -81,7 +144,9 @@ function hgm_styles(){ wp_enqueue_style('hgm_styles', HGM_PLUGIN . 'style.css');
 /* ~~~~~~~~~~~~~~ Actions ~~~~~~~~~~~~~~ */
 
 add_action('wp_footer','hgm_load_api');
+add_action('admin_footer','hgm_load_api');
 add_shortcode('hgm_geocoder','hgm_geo_shortcode');
 add_shortcode('hgm_map','hgm_map_shortcode');
 add_action('wp_print_styles','hgm_styles');
+add_action('admin_print_styles','hgm_styles');
 ?>
